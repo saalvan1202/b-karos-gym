0ef4ee9ea765a88b9b7ff70f15e79ed5819af68a
saalvan1202 <ashande75@gmail.com>
2025-02-13 17:36:14 -0500

    mysql

--- karosgym/settings.py
+++ karosgym/settings.py
@@ -15,6 +15,8 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
