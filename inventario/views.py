be81d13118af19338b090c50dd24098a3a8a3779
saalvan1202 <ashande75@gmail.com>
2025-02-14 00:28:29 -0500

    create-producto

--- inventario/views.py
+++ inventario/views.py
@@ -11,15 +11,26 @@ class ProductosViewSet(viewsets.ModelViewSet):
     serializer_class = ProducotosSerializer
     def perform_create(self, serializer):
         print("Request data:", self.request.data.get('id'))
-        obj=Productos.objects.filter(id=self.request.data.get('id')).first()    
-        if(obj):
+
+    # Crear una copia mutable de los datos del request
+        data = self.request.data.copy()
+        data['id_tipo_productos'] = 123  # Agrega el nuevo valor aquí (cámbialo según sea necesario)
+
+        obj = Productos.objects.filter(id=data.get('id')).first()
+
+        if obj:
+        # Actualizar objeto existente
             for attr, value in serializer.validated_data.items():
                 setattr(obj, attr, value)
-            obj.estado = True # Asegura que el estado sea True en la actualización
+            obj.estado = True  # Asegura que el estado sea True en la actualización
             obj.save()
+            serializer.instance = obj  # Asigna el objeto actualizado al serializer
         else:
-            # Crear (no se proporcionó ID)
-            serializer.save(estado=True)
+        # Serializar los datos con la modificación incluida
+            serializer = self.get_serializer(data=data)
+            serializer.is_valid(raise_exception=True)  # Validar los datos
+            serializer.save(estado=True)  # Guardar el nuevo producto
+
     def destroy(self, request, *args, **kwargs):
         obj=self.get_object()
         obj.estado=False
